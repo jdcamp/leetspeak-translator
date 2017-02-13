76d0abc45ec2919212cd455a927ff860a4c3cc61
Jake Campa and Maggie Harrington <maggie@>
2017-02-13 13:25:42 -0800

    add leetspeak.html.twig and result.html.twig for user interface

--- app/app.php
+++ app/app.php
@@ -1,12 +1,7 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/PingPongGenerator.php";
-
-    session_start();
-    if (empty($_SESSION['leetspeak-session'])) {
-        $_SESSION['leetspeak-session'] = array();
-    }
+    require_once __DIR__."/../src/LeetspeakTranslator.php";
 
     $app = new Silex\Application();
 
@@ -20,11 +15,10 @@
         return $app['twig']->render('leetspeak.html.twig');
     });
 
-    $app->post("/number", function() use ($app) {
-        $newNumber = new PingPongGenerator();
-        $newNumber->generatePingPongArray($_POST['number-input']);
-        $newNumber->save();
-        return $app['twig']->render('number.html.twig', array('newArray' => $newNumber->getOutputString()));
+    $app->post("/result", function() use ($app) {
+        $newString = new LeetspeakTranslator();
+        //$newString->translate($_POST['string-input']);
+        return $app['twig']->render('result.html.twig', array('newArray' => $newString->translate($_POST['string-input'])));
     });
 
     return $app;
