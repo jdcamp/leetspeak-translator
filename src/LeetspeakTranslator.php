bf5d653a872ad46dc8cd687f1466103846f69bd0
Jake Campa and Maggie Harrington <jake@>
2017-02-13 13:13:02 -0800

    pass all conditions in specs

--- src/LeetspeakTranslator.php
+++ src/LeetspeakTranslator.php
@@ -2,15 +2,30 @@
     class LeetspeakTranslator {
         function translate($myInput)
         {
+            $one = 1;
             $leetSpeakArray = array();
             $wordArray = explode(" ", $myInput);
             for ($i=0; $i < sizeof($wordArray); $i++) {
                 $letterArray = str_split($wordArray[$i]);
                 $leetSpeakLetterArray = array();
                 for ($j=0; $j < sizeof($letterArray); $j++) {
+
                     if (strcasecmp($letterArray[$j], 'e') == 0) {
-                        array_push($leetSpeakLetterArray, 3);
-                    } else {
+                        array_push($leetSpeakLetterArray, '3');
+                    } elseif (strcasecmp($letterArray[$j], 'o') == 0) {
+                        array_push($leetSpeakLetterArray, '0');
+                    } elseif ($letterArray[$j] === 'I') {
+                        array_push($leetSpeakLetterArray, '1');
+                    } elseif (strcasecmp($letterArray[$j], 's') == 0) {
+                        if ($j !=0  && ($letterArray[$j-1] == "'" || $letterArray[$j-1] == '"')) {
+                            array_push($leetSpeakLetterArray, $letterArray[$j]);
+                        } elseif ($j == 0) {
+                            array_push($leetSpeakLetterArray, $letterArray[$j]);
+                        } else {
+                            array_push($leetSpeakLetterArray, "z");
+                        }
+                    }
+                        else {
                         array_push($leetSpeakLetterArray, $letterArray[$j]);
                     }
                 }
--- tests/LeetspeakGeneratorTest.php
+++ tests/LeetspeakGeneratorTest.php
@@ -9,13 +9,13 @@
         {
             //Arrange
             $test_LeetspeakTranslator = new LeetspeakTranslator;
-            $input = "trEe";
+            $input = "Don't you love these 'String' exercises? I do!";
 
             //Act
             $result = $test_LeetspeakTranslator->translate($input);
 
             //Assert
-            $this->assertEquals("tr33", $result);
+            $this->assertEquals("D0n't y0u l0v3 th3z3 'String' 3x3rciz3z? 1 d0!", $result);
         }
     }
 
